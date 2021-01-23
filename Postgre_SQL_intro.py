f4b6e1977158bfce7e617b45700011177d39ad36
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2021-01-23 20:46:41 +0530

    sqlite + postgresql

---
+++ Postgre_SQL_intro.py
@@ -0,0 +1,48 @@
+import pyscopg2
+#connect to a database
+#create a  cursor object
+#write an sql query
+#commit changes
+#Close database connections
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?,?,?)",(item, quantity, price))
+    conn.commit()
+    conn.close()
+
+insert("vivek",3,50)
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    conn.commit()
+    conn.close()
+    
+
+def update(quantity,price,item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item = ?", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+# update(5,10,'vivek')
+print(view())
+
